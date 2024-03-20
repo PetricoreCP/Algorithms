b566acea0080101b4ef71c3bb224c9b8fba3e46e
PetricoreCP <156469696+PetricoreCP@users.noreply.github.com>
2024-03-20 13:30:54 +0100

    ModularExponentiation.cpp

--- ModularExponentiation.cpp
+++ ModularExponentiation.cpp
@@ -1,5 +1,3 @@
-#include <bits/stdc++.h>
-
 template <typename T>
 T ModExp(T base, T exp, T mod) {
     T ans = 1;
@@ -13,24 +11,4 @@ T ModExp(T base, T exp, T mod) {
         exp >>= 1;
     }
     return ans % mod;
-}
-
-void Main() {
-    long long base, exp, mod;
-    std::cin >> base >> exp >> mod;
-    long long ans = ModExp(base, exp, mod);
-    std::cout << ans;
-}   
-
-int main() {
-    std::ios::sync_with_stdio(0); 
-    std::cin.tie(0);
-    //freopen("input.txt", "r", stdin);
-    //freopen("output.txt", "w", stdout);
-    int t = 1; 
-    //std::cin >> t;
-    while (t--) {
-        Main();
-    }
-    return 0;
 }
