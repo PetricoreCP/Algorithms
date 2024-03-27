56a1be09a0528230b6ce0406c6dd22e646bf606b
PetricoreCP <156469696+PetricoreCP@users.noreply.github.com>
2024-03-27 11:11:45 +0100

    Disjoint Set Union Data Structure

--- Dsu.cpp
+++ Dsu.cpp
@@ -4,32 +4,42 @@
 class Dsu {
 private:
     std::vector<int> p, sz;
+
 public:
     Dsu(int n) {
         p.resize(n);
         sz.resize(n, 1);
+        
         for(int i = 0; i < n; i ++) {
             p[i] = i;
         }
     }    
+
     int find(int u) {
         int node = u;
+        
         while(p[node] != node) {
             node = p[node];
         }
+        
         while(p[u] != u) {
             int tmp = p[u];
             p[u] = node;
             u = tmp;
         }
+        
         return node;
     }
+
     bool join(int u, int v) {
         int a = find(u), b = find(v);
+        
         if(a == b) return false;
+        
         if(sz[a] < sz[b]) std::swap(a, b);
         sz[a] += sz[b];
         p[b] = a;
+        
         return true;
     }
 };
