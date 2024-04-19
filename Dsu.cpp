65c4aa92c12ae12700442d7db5245dce18f36354
PetricoreCP <156469696+PetricoreCP@users.noreply.github.com>
2024-04-19 09:32:09 +0200

    Disjoint Set Union Data Structure

--- Dsu.cpp
+++ Dsu.cpp
@@ -1,69 +1,52 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 class Dsu {
 private:
-    std::vector<int> p, sz;
-
+    vector<int> p, sz;
 public:
     Dsu(int n) {
         p.resize(n);
         sz.resize(n, 1);
-        
         for(int i = 0; i < n; i ++) {
             p[i] = i;
         }
     }    
-
     int find(int u) {
         int node = u;
-        
         while(p[node] != node) {
             node = p[node];
         }
-        
         while(p[u] != u) {
             int tmp = p[u];
             p[u] = node;
             u = tmp;
         }
-        
         return node;
     }
-
     bool join(int u, int v) {
-        int a = find(u), b = find(v);
-        
+        int a = find(u), b = find(v);   
         if(a == b) return false;
-        
         if(sz[a] < sz[b]) std::swap(a, b);
         sz[a] += sz[b];
         p[b] = a;
-        
         return true;
     }
 };
 
-void Main() {
-    int n;
-    std::cin >> n;
-
-    Dsu dsu(n);
-}
-
 int main() {
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0);
-
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     //freopen("input.txt", "r", stdin);
     //freopen("output.txt", "w", output);
-    
-    int t = 1;
-    //std::cin >> t;
-
-    while(t--) {
-        Main();
+    int tc = 1;
+    //cin >> tc;
+    while(tc--) {
+        int n;
+        cin >> n;
+        Dsu dsu(n);
     }
-
     return 0;
 }
