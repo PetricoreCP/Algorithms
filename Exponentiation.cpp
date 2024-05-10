e4f5f0d0c4d831004ae606780bddbc0cc7d63ff6
PetricoreCP <156469696+PetricoreCP@users.noreply.github.com>
2024-05-10 09:56:10 +0200

    Fast Exponentiation Algorithm

--- Exponentiation.cpp
+++ Exponentiation.cpp
@@ -1,12 +1,5 @@
-#include <iostream>
-
-using namespace std;
-
-const int mod = 1e9 + 7;
-
-template <typename T, typename U, typename V>
-T ModExp(U base, V exp, T mod) {
-    T res = 1;
+long long ModExp(long long base, long long exp, long long mod) {
+    long long res = 1;
     base %= mod;
     while(exp) {
         if(exp & 1) {
@@ -17,19 +10,4 @@ T ModExp(U base, V exp, T mod) {
         exp >>= 1;
     }
     return res % mod;
-}
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    //freopen("input.txt", "r", stdin);
-    //freopen("output.txt", "w", output);
-    int tc = 1;
-    //cin >> tc;
-    while(tc--) {
-        long long base, exp;
-        cin >> base >> exp;
-        cout << ModExp(base, exp, mod);
-    }
-    return 0;
 }
