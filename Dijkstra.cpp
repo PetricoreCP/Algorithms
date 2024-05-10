0a430d72050ae4ca8a43f7cad73bf824d20c6fc2
PetricoreCP <156469696+PetricoreCP@users.noreply.github.com>
2024-05-10 20:06:47 +0200

    Dijkstra's Shortest Path Algorithm

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -2,13 +2,17 @@ vector<long long> Dijkstra(vector<vector<pair<int, long long>>> & adj, int node)
     int n = adj.size();
     vector<long long> cost(n, LLONG_MAX);
     cost[node] = 0;
-    priority_queue<pair<int, long long>, vector<pair<int, long long>>, greater<pair<int, long long>>> pq;
+    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
     pq.push(make_pair(0, node));
     while(!pq.empty()) {
-        auto [currWeight, curr] = pq.top();
+        pair<long long, int> p = pq.top();
+        long long currWeight = p.first;
+        int curr = p.second;
         pq.pop();
         if(currWeight > cost[curr]) continue;
-        for(auto [next, nextWeight] : adj[curr]) {
+        for(pair<int, long long> & q  : adj[curr]) {
+            int next = q.first;
+            long long nextWeight = q.second;
             if(cost[next] > nextWeight + currWeight) {
                 cost[next] = nextWeight + currWeight;
                 pq.push(make_pair(cost[next], next));
