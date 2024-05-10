e180854f9e81652d6f24024f8eb4b597b60d8570
PetricoreCP <156469696+PetricoreCP@users.noreply.github.com>
2024-05-10 10:34:15 +0200

    Fast Exponentiation Algorithm

--- Exponentiation.cpp
+++ Exponentiation.cpp
@@ -1,13 +1,13 @@
-long long ModExp(long long b, long long e, long long m) {
-    long long r = 1;
-    b %= m;
-    while(e) {
-        if(e & 1) {
-            r = (r * b) % m;
-            e --;
+long long ModExp(long long base, long long exp, long long mod) {
+    long long res = 1;
+    base %= mod;
+    while(exp) {
+        if(exp & 1) {
+            res = (res * base) % mod;
+            exp --;
         }
-        b = (b * b) % m;
-        e >>= 1;
+        base = (base * base) % mod;
+        exp >>= 1;
     }
-    return r % m;
+    return res % mod;
 }
