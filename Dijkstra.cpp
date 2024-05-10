3ba6dd684f7b41e77de129b755f9945fbe10340d
PetricoreCP <156469696+PetricoreCP@users.noreply.github.com>
2024-05-10 11:59:02 +0200

    Shortest Path Dijkstra Algorithm

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,50 +1,19 @@
-#include <iostream>
-#include <vector>
-#include <utility>
-#include <queue>
-#include <limits>
-
-using namespace std;
-
-template <typename T, typename U>
-vector <U> Dijkstra(T start, std::vector <std::vector <std::pair <T, U>>> & adj) {
-    vector <U> dist(adj.size(), numeric_limits <U>::max());
-    dist[start] = 0;
-    priority_queue <std::pair <U, T>, vector <pair <U, T>>, greater <pair <U, T>>> pq;
-    pq.push(make_pair(0, start));
+vector<long long> Dijkstra(vector<vector<pair<int, long long>>> & adj, int node) {
+    int n = adj.size();
+    vector<long long> cost(n, LLONG_MAX);
+    cost[node] = 0;
+    priority_queue<pair<int, long long>, vector<pair<int, long long>>, greater<pair<int, long long>>> pq;
+    pq.push(make_pair(0, node));
     while(!pq.empty()) {
-        auto [w, e] = pq.top();
+        auto [currWeight, curr] = pq.top();
         pq.pop();
-        if(w > dist[e]) continue;
-        for(auto [next, nw] : adj[e]) {
-            if(dist[next] > nw + w) {
-                dist[next] = nw + w;
-                pq.push(make_pair(dist[next], next));
+        if(currWeight > cost[curr]) continue;
+        for(auto [next, nextWeight] : adj[curr]) {
+            if(cost[next] > nextWeight + currWeight) {
+                cost[next] = nextWeight + currWeight;
+                pq.push(make_pair(cost[next], next));
             }
         }
     }
-    return dist;
-}
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    //freopen("input.txt", "r", stdin);
-    //freopen("output.txt", "w", output);
-    int tc = 1;
-    //cin >> tc;
-    while(tc--) {
-        int n, m;
-        cin >> n >> m;
-        vector <vector <pair <int, long long>>> adj(n);
-        for(int i = 0; i < m; i ++) {
-            int a, b, w;
-            cin >> a >> b >> w;
-            a --; b --;
-            adj[a].push_back(make_pair(b, w));
-            adj[b].push_back(make_pair(a, w));
-        }
-        vector <long long> dist = Dijkstra(0, adj);
-    }
-    return 0;
+    return cost;
 }
