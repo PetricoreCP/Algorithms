789ec9aaf8e6ba6e716dc5c1eba30dcc20bdc71d
PetricoreCP <156469696+PetricoreCP@users.noreply.github.com>
2024-07-27 23:31:31 +0200

    Binary Exponentiation and Applications

--- binaryExponentiation.cpp
+++ binaryExponentiation.cpp
@@ -10,9 +10,7 @@ long long binExp(long long base, long long exp) {
 long long binExp(long long base, long long exp) {
 	long long ans = 1;
 	while(exp) {
-		if(exp & 1) {
-			ans *= base;
-		}
+		if(exp & 1) ans *= base;
 		base *= base;
 		exp >>= 1;
 	}
@@ -24,11 +22,28 @@ long long binModExp(long long base, long long exp, long long mod) {
 	long long ans = 1;
 	base %= mod;
 	while(exp) {
-		if(exp & 1) {
-			ans = ans * base % mod;
-		}
+		if(exp & 1) ans = ans * base % mod;
 		base = base * base % mod; 
 		exp >>= 1;
 	}
 	return ans;
+}
+
+// Applying a permutation k times
+vector<long long> apply(vector<long long> sequence, vector<long long> permutation) {
+	int n = sequence.size();
+    vector<long long> res(n);
+    for(int i = 0; i < n; i ++) {
+        res[i] = sequence[permutation[i]];
+    }
+    return res;
+}
+
+vector<long long> permute(vector<long long> sequence, vector<long long> permutation, long long k) {
+    while(k) {
+        if(k & 1) sequence = apply(sequence, permutation);
+        permutation = apply(permutation, permutation);
+        k >>= 1;
+    }
+    return sequence;
 }
