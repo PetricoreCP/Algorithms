999a8de84c618d766831e4653a35d624cbb2ae84
PetricoreCP <156469696+PetricoreCP@users.noreply.github.com>
2024-10-02 09:43:52 +0200

    Shortest Path Algorithm: Bellman-Ford

--- bellmanFord.cpp
+++ bellmanFord.cpp
@@ -3,10 +3,28 @@ using namespace std;
 using ll = long long;
 const ll INF = 1e18;
 
+vector<bool> markCycle(vector<tuple<int, int, ll>> &edges, vector<ll> &distance, int n) {
+    vector<bool> negativeCycle(n);
+    for(int i = 0; i < n; i ++) {
+        for(auto &edge : edges) {
+            int u, v;
+            ll w;
+            tie(u, v, w) = edge;
+            if(distance[u] != INF && distance[v] > distance[u] + w) {
+                negativeCycle[u] = true;
+                negativeCycle[v] = true;
+            }
+            if(negativeCycle[u]) {
+                negativeCycle[v] = true;
+            }
+        }
+    }
+    return negativeCycle;
+}
+
 vector<ll> bellmanFord(vector<tuple<int, int, ll>> &edges, int n, int start) {
     vector<ll> distance(n, INF);
     distance[start] = 0;
-    bool detection = false;
     for(int i = 0; i < n - 1; i ++) {
         bool converged = true;
         for(auto &edge : edges) {
@@ -20,16 +38,10 @@ vector<ll> bellmanFord(vector<tuple<int, int, ll>> &edges, int n, int start) {
         }
         if(converged) break;
     }
-    for(auto &edge : edges) {
-        int u, v;
-        ll w;
-        tie(u, v, w) = edge;
-        if(distance[v] > distance[u] + w) {
-            detection = true;
-            break;
-        }
+    vector<bool> negativeCycle = markCycle(edges, distance, n);
+    for(int i = 0; i < n; i ++) {
+        if(negativeCycle[i]) distance[i] = -INF;
     }
-    if(detection) distance[start] = -1;
     return distance;
 }
 
@@ -45,12 +57,9 @@ void testCase() {
     }
     int start = 0;
     vector<ll> distance = bellmanFord(edges, n, start);
-    if(distance[start] == -1) {
-        cout << "Negative Cycle Detected\n";
-        return;
-    }
     for(int i = 0; i < n; i ++) {
         if(distance[i] == INF) cout << "Unreachable ";
+        else if(distance[i] == -INF) cout << "NegativeCycle "; 
         else cout << distance[i] << ' ';
     }
     cout << '\n';
