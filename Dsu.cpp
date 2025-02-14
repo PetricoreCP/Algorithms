4fc0d62d3e71db80d2b24eca98c2373be9947427
PetricoreCP <156469696+PetricoreCP@users.noreply.github.com>
2025-02-14 22:38:55 +0100

    Dsu

--- Dsu.cpp
+++ Dsu.cpp
@@ -35,17 +35,24 @@ public:
 };
 
 struct Dsu {
+    int n, cnt;
     vector<int> parent, sz;
-    Dsu(int n) {
+    Dsu(int n) : n(n), cnt(n) {
         parent.resize(n);
         sz.resize(n, 1);
         iota(parent.begin(), parent.end(), 0);
     }
     int find(int u) {
-        while (parent[u] != u) {
-            parent[u] = find(parent[u]);
+        int root = u;
+        while (parent[root] != root) {
+            root = parent[root];
         }
-        return parent[u];
+        while (parent[u] != u) {
+            int tmp = parent[u];
+            parent[u] = root;
+            u = tmp;
+        }
+        return root;
     }
     bool join(int u, int v) {
         u = find(u);
@@ -58,9 +65,16 @@ struct Dsu {
         }
         sz[u] += sz[v];
         parent[v] = u;
+        cnt --;
         return true;
     }
     bool same(int u, int v) {
         return find(u) == find(v);
+    }
+    int getSize(int u) {
+        return sz[find(u)];
+    }
+    int countComponents() {
+        return cnt;
     }
 };
